97a81dee42b6cd32468df84640cd6ba24d61f518
Qoosat <nkl.rif@gmail.com>
2024-01-20 15:22:01 +0300

    prikol

--- utils/browser_setup.py
+++ utils/browser_setup.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 browser_width = int(os.getenv('BROWSER_WIDTH', '800'))
 browser_height = int(os.getenv('BROWSER_HEIGHT', '600'))
-headless_mode = os.getenv('HEADLESS', 'True').lower() == 'true'
+headless_mode = bool(os.getenv('HEADLESS', 'True'))
 
 
 class BrowserSetup:
